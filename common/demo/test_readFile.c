19a519e88f0a2b5e6a3cf44f59a7d7ae1c747728
fabianmuehlberger <muehlberger.fabian@gmail.com>
2020-06-03 13:11:09 +0200

    seperating funktions

--- common/demo/test_readFile.c
+++ common/demo/test_readFile.c
@@ -5,41 +5,63 @@
 #include "my_structure.h"
 #include "my_file_io.h"
 
+void openFile(char *fileName);
+
 void readFile(char *fileName);
+
+void createArray(FILE *dataArray, int lineCount);
+
+int checkLineToken(char *fileLine);
 
 int main(void)
 {
     printf("WELCOME TO TEST_READFILE.C\n");
-/*
+
     char fileName[NAMELEN];
     char fileNameAndPath[NAMELEN];
 
     getUserInputStringFromConsole(fileName);
-    strcpy(fileNameAndPath, ".\\\\ressources\\\\");
+    //strcpy(fileNameAndPath, ".\\\\ressources\\\\");
 
     strcat(fileNameAndPath, fileName);
-    printf("filename = %s\n", fileNameAndPath);
-*/
+    printf("fileAndPath = %s\n", fileNameAndPath);
 
-    char fileName[NAMELEN] = ".\\ressources\\test_data_short.csv";
+    //char fileName[NAMELEN] = ".\\ressources\\test_data_short.csv";
 
     char stringToken = ';';
 
-    //help to count lines
-    
-    //check current Direktory
-    //checkDirectory();
+    //check Line Count of opened File
+    int lineCount = checkLineCount(fileNameAndPath);
+    printf("LineCount = %i\n", lineCount);
+
+    //checkl File Size of opened File
+    int fileSize = checkFileSize(fileNameAndPath);
+    printf("Filesize = %d\n", fileSize);
 
     //open and close File
-    readFile(fileName);
-
-    
+    readFile(fileNameAndPath);
 }
 
+// !when finished! put this funktion in my_file_io
 void readFile(char *fileName)
 {
     char buffer[FILEBUFFER];
-    
+    FILE *pFile;
+    openFile(pFile);
+
+    //print the values of the file
+    while (fgets(buffer, FILEBUFFER, pFile) != NULL)
+    {
+        checkLineToken(buffer);
+        printf("%s", buffer);
+    }
+
+    fclose(pFile);
+}
+
+// !when finished with readfile()! put this function in my_file_io
+void openFile(char *fileName)
+{
 
     FILE *pFile;
     pFile = fopen(fileName, "r");
@@ -52,23 +74,40 @@ void readFile(char *fileName)
     {
         printf("File opened...\n");
     }
-
-    //check Line Count of opened File
-    int lineCount = checkLineCount(fileName);
-    printf("LineCount = %i\n", lineCount);
-
-    //checkl File Size of opened File
-    int fileSize = checkFileSize(fileName);
-    printf("Filesize = %d\n", fileSize);
-
-
-    //print the values of the file 
-    while (fgets(buffer, FILEBUFFER, pFile) != NULL)
-    {
-        printf("%s", buffer);
-    }
-
-
-    fclose(pFile);
 }
 
+// !when finished!, put this funktion in my_file_io
+void createArray(FILE *dataArray, int lineCount)
+{
+    Data *tmp = malloc(lineCount * sizeof(Data));
+    if (tmp == NULL)
+    {
+        printf("NO MEMORY ALLOCATED");
+        exit(-1);
+    }
+    dataArray = tmp;
+}
+
+// !when finished! put this funktion in my_validation
+int checkLineToken(char *fileLine)
+{
+    int lenght = strlen(fileLine);
+    int tokenCount = 0;
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if (fileLine[i] == ';')
+        {
+            tokenCount++;
+        }
+    }
+    //check for the right amount of ';' in the Line
+    if (tokenCount < FILE_TOKEN_AMOUNT)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
--- common/include/my_validation.h
+++ common/include/my_validation.h
@@ -1,21 +1,22 @@
 
 #ifndef _MY_VALIDATION_H_
 #define _MY_VALIDATION_H_
-//test my_validation
+
+#define FILE_TOKEN_AMOUNT 10
+#define STRING_TOKEN ';'
 
 //simple test funktion to show that my_validation exitst
 int my_validation_Test();
 
 // serches through file to check lines of the File
-// returns a integer with number if Lines 
+// returns a integer with number if Lines
 int checkLineCount(char *pFile);
 
 // returns size of File in bytes
 int checkFileSize(char *pFileSize);
 
-//prints current directory with its files 
+//prints current directory with its files
 // might be usefull to find a file
 void checkDirectory();
-
 
 #endif
