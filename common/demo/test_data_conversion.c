c4e8f5f97463bc17b8b05f464453654029fdf133
fabianmuehlberger <muehlberger.fabian@gmail.com>
2020-06-11 23:05:40 +0200

    test_data_conversion: partly working solo prototyp

--- common/demo/test_data_conversion.c
+++ common/demo/test_data_conversion.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-#define STRINGLEN 256
+//DEFINES
 
-typedef struct Data
+#define DT 0.02 // [s/loop] loop period. 20ms
+#define AA 0.97 // complementary filter constant
+
+#define A_GAIN 0.0573 // [deg/LSB]
+#define G_GAIN 0.070  // [deg/s/LSB]
+#define RAD_TO_DEG 57.29578
+#define M_PI 3.14159265358979323846
+
+//#define ACC_GAIN 0.732 // [mg/LBS] (FS:+-16g)
+//#define MAG_GAIN 0.43 // [mgauss/LBS] (FS:+-12gauss)
+//#define GYRO_GAIN 70 // [mdps/LBS] (FS:+-2000dps)
+
+typedef struct Raw_Data
 {
     int index;
     int validationCheck;
@@ -16,59 +29,127 @@ typedef struct Data
 
     int gyro_X;
     int gyro_Y;
-    int gyro_z;
+    int gyro_Z;
 
     int mag_X;
     int mag_Y;
     int mag_Z;
 
-} Data;
+} Raw_Data;
 
-Data writeDataToStructureArray(char *buffer);
+typedef struct Clean_Data
+{
+    float gyroXangle;
+    float gyroYangle;
+    float gyroZangle;
 
-void printSomeData(Data someData);
+    float AccYangle;
+    float AccXangle;
 
-void printAnoterString(char *anotherString);
+    float CFangleX;
+    float CFangleY;
+
+} Clean_Data;
+
+Raw_Data readRawDataFromCSV(char *buffer);
+Clean_Data dataConversion(Raw_Data rawData);
+
+void printRawData(Raw_Data someData);
+void printCleanData(Clean_Data c_data);
+
+void changeAccAxis(Clean_Data *tmp);
 
 int main(void)
 {
-    char string[STRINGLEN];
+    char string[200];
+    strcpy(string, "1;0;00000005;fffffff3;ffffffe7;0000056e;ffffffee;ffffffdd;fffffff7;000001e8;000000e8;fffffba4");
 
-    char *pEnd;
-    char *curser;
+    Raw_Data dataPoint;
+    Clean_Data theData;
 
-    Data somedata;
+    dataPoint = readRawDataFromCSV(string);
 
-    char anotherString[10] = "ffffff4";
-    curser = anotherString;
+    theData = dataConversion(dataPoint);
 
-    strcpy(string, "1;0;5;0;fffffff4;569;ffffffea;ffffffd9;fffffff5;0000019c;0000004b;fffffb8c");
-
-    printf("string = %s\n", string);
-    printf("anotherString = %s\n", anotherString);
-
-    //CONVERSION:
-
-    somedata = writeDataToStructureArray(string);
-
-    printSomeData(somedata);
-    return 0;
-}
-void printAnoterString(char *anotherString)
-{
-    char *curser;
-    curser = anotherString;
-
-    long int number;
-
-    number = strtol(curser, NULL, 16);
-
-    printf("%lu", number);
+    printRawData(dataPoint);
+    printCleanData(theData);
 }
 
-Data writeDataToStructureArray(char *buffer)
+Clean_Data dataConversion(Raw_Data rawData)
 {
-    Data tmpData;
+    Clean_Data tmpData;
+
+    float rate_gyr_y = 0.0; // [deg/s]
+    float rate_gyr_x = 0.0; // [deg/s]
+    float rate_gyr_z = 0.0; // [deg/s]
+
+    //Convert Gyro raw to degrees per second
+    rate_gyr_x = (float)rawData.gyro_X * G_GAIN;
+    rate_gyr_y = (float)rawData.gyro_Y * G_GAIN;
+    rate_gyr_z = (float)rawData.gyro_Z * G_GAIN;
+
+    //Calculate the angles from the gyro
+    tmpData.gyroXangle += rate_gyr_x * DT;
+    tmpData.gyroYangle += rate_gyr_y * DT;
+    tmpData.gyroZangle += rate_gyr_z * DT;
+
+    //Convert Accelerometer values to degrees
+    tmpData.AccXangle = (float)(atan2(rawData.acc_Y, rawData.acc_Z) + M_PI) * RAD_TO_DEG;
+    tmpData.AccYangle = (float)(atan2(rawData.acc_Z, rawData.acc_X) + M_PI) * RAD_TO_DEG;
+
+    changeAccAxis(&tmpData);
+    
+    //Complementary filter used to combine the accelerometer and gyro values.
+    tmpData.CFangleX = AA * (tmpData.CFangleX + rate_gyr_x * DT) + (1 - AA) * tmpData.AccXangle;
+    tmpData.CFangleY = AA * (tmpData.CFangleY + rate_gyr_y * DT) + (1 - AA) * tmpData.AccYangle;
+
+
+    return tmpData;
+}
+
+void changeAccAxis(Clean_Data *tmp)
+{
+    //Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up.
+    //Two different pieces of code are used depending on how your IMU is mounted.
+    //If IMU is upside down
+
+    //If IMU is up the correct way, use these lines
+    tmp->AccXangle -= (float)180.0;
+    if (tmp->AccYangle > 90)
+        tmp->AccYangle -= (float)270;
+    else
+        tmp->AccYangle += (float)90;
+
+    /*
+	if (AccXangle >180)
+		AccXangle -= (float)360.0;
+
+		AccYangle-=90;
+	if (AccYangle >180)
+			AccYangle -= (float)360.0;
+    */
+}
+
+void printRawData(Raw_Data data)
+{
+    printf("Acc: %i %i %i  | Gyro: %i %i %i  | Magn: %i %i %i\n",
+           data.acc_X, data.acc_Y, data.acc_Z,
+           data.gyro_X, data.gyro_Y, data.gyro_Z,
+           data.mag_X, data.mag_Y, data.mag_Z);
+}
+
+void printCleanData(Clean_Data data)
+{
+    printf("Gyro: %f %f %f  | Acc: %f %f   | CFangle: %f %f \n",
+           data.gyroXangle, data.gyroYangle, data.gyroZangle,
+           data.AccXangle, data.AccYangle,
+           data.CFangleX, data.CFangleY);
+}
+
+
+Raw_Data readRawDataFromCSV(char *buffer)
+{
+    Raw_Data tmpData;
 
     char *curser;
     char seperator[] = ";";
@@ -83,179 +164,31 @@ Data writeDataToStructureArray(char *buffer)
     tmpData.timeStampMS = (int)strtol(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.acc_X = (int)strtol(curser, NULL, 16);
+    tmpData.acc_X = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.acc_Y = (int)strtol(curser, NULL, 16);
+    tmpData.acc_Y = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.acc_Z = (int)strtol(curser, NULL, 16);
+    tmpData.acc_Z = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.gyro_X = (int)strtol(curser, NULL, 16);
+    tmpData.gyro_X = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.gyro_Y = (int)strtol(curser, NULL, 16);
+    tmpData.gyro_Y = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.gyro_z = (int)strtol(curser, NULL, 16);
+    tmpData.gyro_Z = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.mag_X = (int)strtol(curser, NULL, 16);
+    tmpData.mag_X = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.mag_Y = (int)strtol(curser, NULL, 16);
+    tmpData.mag_Y = (int)strtoul(curser, NULL, 16);
 
     curser = strtok(NULL, seperator);
-    tmpData.mag_Z = (int)strtol(curser, NULL, 16);
+    tmpData.mag_Z = (int)strtoul(curser, NULL, 16);
 
     return tmpData;
-}
-
-void printSomeData(Data someData)
-{
-    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",
-           someData.index,
-           someData.validationCheck,
-           someData.timeStampMS,
-           someData.acc_X,
-           someData.acc_Y,
-           someData.acc_Z,
-           someData.gyro_X,
-           someData.gyro_Y,
-           someData.gyro_z,
-           someData.mag_X,
-           someData.mag_Y,
-           someData.mag_Z);
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "config.h"
-#define DT 0.02
-
-typedef struct Data
-{
-    int index;
-    int validationCheck;
-    int timeStampMS;
-
-    float acc_X;
-    float acc_Y;
-    float acc_Z;
-
-    float gyro_X;
-    float gyro_Y;
-    float gyro_Z;
-
-    float mag_X;
-    float mag_Y;
-    float mag_Z;
-
-} Data;
-
-Data writeDataToStructureArray(char *buffer);
-
-void printSomeData(Data someData);
-
-Data dataConversion(int raw_acc[], int raw_gyro[] int raw_magn[]);
-
-int main(void)
-{
-    printf("Start of main\n");
-
-    char string[200];
-
-    strcpy(string, "1;0;00000005;fffffff3;ffffffe7;0000056e;ffffffee;ffffffdd;fffffff7;000001e8;000000e8;fffffba4");
-
-    Data somedata;
-
-    somedata = writeDataToStructureArray(string);
-    printSomeData(somedata);
-
-}
-
-Data writeDataToStructureArray(char *buffer)
-{
-    Data tmpData;
-
-    int index;
-    int timeStamp;
-    int validationCheck;
-
-    int acc[3];
-    int gyro[3];
-    int magn[3];
-
-    char *curser;
-    char seperator[] = ";";
-
-    curser = strtok(buffer, seperator);
-    tmpData.index = atof(curser);
-
-    curser = strtok(NULL, seperator);
-    validationCheck = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    timeStamp = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    acc[0] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    acc[1] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    acc[2] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    gyro[0] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    gyro[1] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    gyro[2] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    magn[0] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    magn[1] = (int)strtol(curser, NULL, 16);
-
-    curser = strtok(NULL, seperator);
-    magn[2] = (int)strtol(curser, NULL, 16);
-
-    dataConversion(acc, gyro, magn);
-
-    return tmpData;
-}
-
-//Calculate the angles from the gyro
-Data dataConversion(int raw_acc[], int raw_gyro[] int raw_magn[])
-{
-    Data someData;
-
-    someData.gyro_X += raw_gyro[1] * DT;
-    someData.gyro_Y += raw_gyro[2] * DT;
-    someData.gyro_Z += raw_gyro[3] * DT;
-    return someData;
-}
-
-void printSomeData(Data someData)
-{
-    printf("\nIndex: %li\nValid: %li\n\n Time: %li\n acc_x: %li\n acc_y: %li\n acc_z: %li\n\n gyro_x: %li\n gyro_y: %li\n gyro_z:%li\n\n mag_x: %li\n mag_y: %li\n mag_z: %li\n",
-           someData.index,
-           someData.validationCheck,
-           someData.timeStampMS,
-           someData.acc_X,
-           someData.acc_Y,
-           someData.acc_Z,
-           someData.gyro_X,
-           someData.gyro_Y,
-           someData.gyro_Z,
-           someData.mag_X,
-           someData.mag_Y,
-           someData.mag_Z);
 }
